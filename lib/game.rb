faa35bd88e8f26dccff114983bbfce463430e593
masha <fppl0101@gmail.com>
2022-08-05 18:22:25 +0300

    refactor

--- lib/game.rb
+++ lib/game.rb
@@ -1,31 +1,37 @@
+at_exit { puts 'Success!' }
+
 class Game
   attr_reader :max_attempts, :max_number
 
-  def initialize(max_attempts:, max_number:)
+  def initialize(max_attempts: 3, max_number: 10)
     @max_attempts = max_attempts
     @max_number = max_number
     @try = 0
-    @guess_number = rand(1..@max_number)
+    @guess_number = rand(1..max_number)
   end
+
+
+  def guessing_number
+    while guessing?
+      puts 'Enter the number:'
+      guess = Integer(gets, exception: false)
+      next unless guess.is_a?(Integer)
+
+      break if check_guess(guess)
+      @try += 1
+    end
+    puts 'You have lost!'
+  end
+
+  private
 
   def guessing?
     @try < @max_attempts
   end
 
-  def guessing_number
-    while guessing?
-      puts 'Enter the number:'
-      guess = gets.chomp.to_i
-      if guess > @guess_number
-        puts 'Your number is too high'
-      elsif guess < @guess_number
-        puts 'Your number is too low'
-      else
-        puts 'Success!'
-        exit
-      end
-    @try += 1
-    end
-    puts 'You have lost!'
+  def check_guess(guess)
+    exit if guess == @guess_number
+
+    puts "Your number is too #{(guess <=> @guess_number) > 0 ? 'high' : 'low'}"
   end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-require './lib/game'
+require_relative 'lib/game'
 
-game = Game.new(max_attempts: 3, max_number: 10)
+game = Game.new(max_attempts: 5, max_number: 10)
 
 puts "Welcome to the game 'Guess the number'! The number is between 1 and #{game.max_number}. You have only #{game.max_attempts} attempts to guess the number. Good luck!"
 
